d37c39282012e2a42f55baf0a0c710f2ffd29d75
Ben Stein <bumstain@gmail.com>
2015-07-05 20:00:13 +0100

    Added functionality to scrape listings of marketplace

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,21 @@
 require 'open-uri'
 require 'nokogiri'
 
-url = "https://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
+# Store URL to be scraped
+url = "https://www.airbnb.co.uk/s/Brooklyn--NY--United-states"
+
+# Parse the page with Nokogiri
 page = Nokogiri::HTML (open(url))
 
-page.css('td[style="text-align:left;"]').each do |line|
+# Display output onto the screen
+page.css('div.h5.listing-name').each do |line|
+	puts line.text
+end
+
+page.css('span.h3.price-amount').each do |line|
+	puts line.text
+end
+
+page.css('div.text-muted.listing-location.text-truncate').each do |line|
 	puts line.text
 end
