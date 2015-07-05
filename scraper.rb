37092eea877e504adde8d6fbbdea6fc723199a59
Ben Stein <bumstain@gmail.com>
2015-07-05 20:21:49 +0100

    Extract data to CSV file

--- scraper.rb
+++ scraper.rb
@@ -1,21 +1,34 @@
-require 'open-uri'
 require 'nokogiri'
-
+require 'open-uri'
+require 'csv'
+ 
 # Store URL to be scraped
-url = "https://www.airbnb.co.uk/s/Brooklyn--NY--United-states"
-
+url = "https://www.airbnb.com/s/Brooklyn--NY--United-States"
+ 
 # Parse the page with Nokogiri
-page = Nokogiri::HTML (open(url))
+page = Nokogiri::HTML(open(url))
 
-# Display output onto the screen
+# Store data in arrays
+name = []
 page.css('div.h5.listing-name').each do |line|
-	puts line.text
+  name << line.text
 end
 
+price = []
 page.css('span.h3.price-amount').each do |line|
-	puts line.text
+  price << line.text
 end
 
+details = []
 page.css('div.text-muted.listing-location.text-truncate').each do |line|
-	puts line.text
+  details << line.text
+end
+
+# Write data to CSV file
+CSV.open("airbnb_listings.csv", "w") do |file|
+  file << ["Listing Name", "Price", "Details"]
+
+  name.length.times do |i|
+    file << [name[i], price[i], details[i]]
+  end
 end
