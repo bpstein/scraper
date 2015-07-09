8b9e9c48864942853349902b37d12c68a489348d
Ben Stein <bumstain@gmail.com>
2015-07-10 00:14:43 +0100

    Refactored code for multiple page scraper

--- scraper.rb
+++ scraper.rb
@@ -8,34 +8,46 @@ url = "https://www.airbnb.com/s/Brooklyn--NY--United-States"
 # Parse the page with Nokogiri
 page = Nokogiri::HTML(open(url))
 
+# Scrape the maximum number of pages and store in max_range variable
 page_numbers = []
 page.css("div.pagination ul li a[target]").each do |line|
 	page_numbers << line.text
 end
 
-puts page_numbers
+max_page = page_numbers.max
 
-# Store data in arrays
+# Initialize empty arrays
 name = []
-page.css('div.h5.listing-name').each do |line|
-  name << line.text.strip
-end
-
 price = []
-page.css('span.h3.price-amount').each do |line|
-  price << line.text
-end
-
 details = []
-page.css('div.text-muted.listing-location.text-truncate').each do |line|
-  details << line.text.strip.split(/ /)
+
+# Loop once for every page of search results
+max_page.to_i.times do |i|
+
+	# Open search results page
+	url = "https://www.airbnb.com/s/Brooklyn--NY--United-States?page=#{i+1}"
+	page = Nokogiri::HTML(open(url))
+
+	# Store data in arrays
+	page.css('div.h5.listing-name').each do |line|
+	  name << line.text.strip
+	end
+
+	page.css('span.h3.price-amount').each do |line|
+	  price << line.text
+	end
+
+	page.css('div.text-muted.listing-location.text-truncate').each do |line|
+	  details << line.text.strip.split(/ /)
+	end
+
 end
 
-# Write data to CSV file
-# CSV.open("airbnb_listings.csv", "w") do |file|
-#   file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
+#Write data to CSV file
+CSV.open("airbnb_listings.csv", "w") do |file|
+  file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
 
-#   name.length.times do |i|
-#     file << [name[i], price[i][0], details[i][1], details[i][2]]
-#   end
-# end
+  name.length.times do |i|
+    file << [name[i], price[i][0], details[i][1], details[i][2]]
+  end
+end
